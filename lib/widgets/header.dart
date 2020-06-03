4bb2a7bde64f534206e54306321c31d29aa9d61a
syamgith <syamgithksudheesh@gmail.com>
2020-06-04 01:02:23 +0530

    setup profile page added

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
-header() {
-  return Text("header");
+AppBar header(context,
+    {bool isApptitle = false, String title, bool removebackBtn = false}) {
+  return AppBar(
+    automaticallyImplyLeading: removebackBtn ? false : true,
+    backgroundColor: Theme.of(context).primaryColor,
+    centerTitle: true,
+    title: Text(
+      isApptitle ? 'Friendsgram' : title,
+      style: TextStyle(
+        fontSize: isApptitle ? 50 : 40,
+        fontFamily: isApptitle ? 'signatra' : 'signatra',
+      ),
+    ),
+  );
 }
