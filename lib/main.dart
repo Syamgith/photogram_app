02bc2424f8b8c2f7183570be8b495c0255d35616
syamgith <syamgithksudheesh@gmail.com>
2020-06-03 19:19:16 +0530

    added firestore and depentencies

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'PhotoGram',
       theme: ThemeData(
-          primarySwatch: Colors.purple, accentColor: Colors.greenAccent),
+          primarySwatch: Colors.pink, accentColor: Colors.greenAccent),
       debugShowCheckedModeBanner: false,
       home: Home(),
     );
